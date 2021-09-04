f0c3a1d540cc11e3cae9a8da4e442b5910c4a018
Abu Khalid <brengozbjm@gmail.com>
2021-09-04 17:23:03 +0800

    addadsterra

---
+++ ads/safira1.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '1c2f02588ccfb99967f45345586493c8',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://gadsatz.com/1c2f02588ccfb99967f45345586493c8/invoke.js"></scr' + 'ipt>');
---
+++ ads/zulfa2.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '671c97bcdbb1f9731255757970154076',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://2e8dgn8n0e0l.com/671c97bcdbb1f9731255757970154076/invoke.js"></scr' + 'ipt>');
